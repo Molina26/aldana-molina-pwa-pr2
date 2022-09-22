bf86ce85fac5d4f6259d15dad234de7961f4aad5
Manuel A. Molina <68267658+Molina26@users.noreply.github.com>
2022-09-22 16:41:08 -0500

    change path access

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if (navigator.serviceWorker) {
     const enviroment = window.location.href;
-    const pathInit = enviroment.includes('localhost') ? '/sw.js/'
-    navigator.serviceWorker.register('/sw.js');
+    const pathInit = enviroment.includes('localhost') ? '/sw.js' : 'https://molina26.github.io/aldana-molina-pwa-pr2/sw.js'
+    navigator.serviceWorker.register(pathInit);
 }
 
